27e02009a0e71e407aa0a38141b86b8413742075
Richard Kasperowski <r@kasperowski.com>
2022-03-06 16:39:46 -0500

    Refactor: hard-coded strings

--- cv-creator.py
+++ cv-creator.py
@@ -3,30 +3,38 @@ from pdf2docx import parse
 from weasyprint import HTML, CSS
 from weasyprint.text.fonts import FontConfiguration
 
+WEB_CSS_FILE = 'static/web.css'
+
+PRINT_CSS_FILE = 'static/print.css'
+
+MARKDOWN_FILE = 'cv.md'
 
 OUTPUT_DIR = 'generated'
+DOCX_FILE = OUTPUT_DIR + '/cv.docx'
+HTML_FILE = OUTPUT_DIR + '/cv.html'
+PDF_FILE = OUTPUT_DIR + '/cv.pdf'
 
 if __name__ == '__main__':
     # Convert Markdown file to simple HTML
-    with open('cv.md', 'r') as f:
+    with open(MARKDOWN_FILE, 'r') as f:
         text = f.read()
         simple_html = markdown.markdown(text, extensions=['smarty'])
 
     # Convert to PDF and output PDF file
     font_config = FontConfiguration()
-    css = CSS('static/print.css', font_config=font_config)
+    css = CSS(PRINT_CSS_FILE, font_config=font_config)
     parsed_html = HTML(string=simple_html)
-    parsed_html.write_pdf(OUTPUT_DIR + '/cv.pdf', stylesheets=[css], font_config=font_config)
+    parsed_html.write_pdf(PDF_FILE, stylesheets=[css], font_config=font_config)
 
     # Enhance simple HTML and output HTML file
     full_html = '''<!DOCTYPE html>
 <head>
   <title>Richard Kasperowski - Curriculum Vitae</title>
-  <link rel="stylesheet" href="../static/web.css">
+  <link rel="stylesheet" href="../''' + WEB_CSS_FILE + '''">
 </head>
 <body>''' + simple_html + '''</body></html>'''
-    with open(OUTPUT_DIR + '/cv.html', 'w') as f:
+    with open(HTML_FILE, 'w') as f:
         f.write(full_html)
 
     # Convert PDF to Word file
-    parse(pdf_file=OUTPUT_DIR + '/cv.pdf', docx_with_path=OUTPUT_DIR + '/cv.docx')
+    parse(pdf_file=PDF_FILE, docx_with_path=DOCX_FILE)
